aac4d453aa18e5a6f297c73771afeaa06d8d499b
gunawanzakiahhalim <gunawanzakiahhalim@gmail.com>
2021-02-25 17:05:05 +0700

    modified file.php

--- app/Views/info/file.php
+++ app/Views/info/file.php
@@ -1,3 +1,55 @@
+<div class="row mb-3">
+  <div class="col-md-6">
+    <div class="card shadow">
+      <h5 class="card-header"><a href="https://kemitraan.pertamina.com/" target="_blank" class="text-decoration-none">Kemitraan Pertamina</a></h5>
+      <div class="card-body">
+        <h5 class="card-title">Info</h5>
+        <div class="list-group mb-3">
+          <a href="https://kemitraan.pertamina.com/dashboard/info.html" target="_blank" class="list-group-item list-group-item-action list-group-item-info">SPBU</a>
+          <a href="https://kemitraan.pertamina.com/dashboard/info/pertashop.html" target="_blank" class="list-group-item list-group-item-action list-group-item-info">PERTASHOP</a>
+          <a href="https://kemitraan.pertamina.com/dashboard/info/satu-harga.html" target="_blank" class="list-group-item list-group-item-action list-group-item-info">BBM SATU HARGA</a>
+          <a href="https://kemitraan.pertamina.com/dashboard/info/spbe-pso.html" target="_blank" class="list-group-item list-group-item-action list-group-item-info">SPBE PSO</a>
+          <a href="https://kemitraan.pertamina.com/dashboard/info/spbe-npso.html" target="_blank" class="list-group-item list-group-item-action list-group-item-info">SPBE NON PSO</a>
+          <a href="https://kemitraan.pertamina.com/dashboard/info/bengkel-pemeliharaan-tabung-lpg-pso.php.html" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Bengkel Pemeliharaan Tabung LPG 3 kg</a>
+          <a href="https://kemitraan.pertamina.com/dashboard/info/bengkel-pemeliharaan-tabung-lpg-npso.php.html" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Bengkel Pemeliharaan Tabung LPG Non PSO</a>
+          <a href="https://kemitraan.pertamina.com/dashboard/info/keagenan-lpg-pso.html" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Keagenan LPG PSO</a>
+          <a href="https://kemitraan.pertamina.com/dashboard/info/keagenan-lpg-npso.html" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Keagenan LPG Non PSO</a>
+        </div>
+        <a href="https://kemitraan.pertamina.com/dashboard/procedure.html" target="_blank" class="btn btn-primary">Prosedur</a>
+        <a href="https://kemitraan.pertamina.com/dashboard/faq.html" target="_blank" class="btn btn-primary">Tanya Jawab</a>
+
+      </div>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <div class="card shadow">
+      <h5 class="card-header">Download Files</h5>
+      <div class="card-body">
+        <h5 class="card-title">Excel</h5>
+        <div class="list-group">
+          <a href="https://spbumypertamina.000webhostapp.com/xlsx/Rekap%20SPBU%20MyPertamina%2022.02.21.xlsx" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Rekap SPBU MyPertamina 22.02.21
+            <?php $timeTarget = 1614826654; ?>
+            <?php if ($timeTarget > time()) : ?>
+              <span class="badge bg-success">New</span>
+            <?php endif; ?>
+          </a>
+          <a href="https://chanelsosmed.000webhostapp.com/xlsx/Arjuna%20-%20Updates%2018%20Februari%202021.xlsx" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Arjuna - Updates 18 Februari 2021
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-circle" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" />
+            </svg>
+          </a>
+          <a href="https://spbumypertamina.000webhostapp.com/xlsx/list%20spbu%20produk.xlsx" target="_blank" class="list-group-item list-group-item-action list-group-item-info">List SPBU Produk
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-circle" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" />
+            </svg>
+          </a>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 <div class="row">
   <div class="col-md-6">
     <div class="card shadow">
@@ -5,13 +57,38 @@
         <h5 class="card-title text-center">Informasi</h5>
         <div class="list-group">
           <h6 class="card-subtitle mb-2 text-muted mt-3">**Informasi Lengkap cek Diana</h6>
+          <a href="https://arjunadigital.000webhostapp.com/pdf/Materi%20Upskilling%20PCC%201-500-000.pdf" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Materi Upskilling PCC 1-500-000</a>
+          <a href="https://ops11.000webhostapp.com/img/Lokasi%20Penukaran%20Pelumas.jpeg" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Lokasi Penukaran Pelumas</a>
           <a href="https://ops11.000webhostapp.com/img/Lokasi%20SPBU%20Terdampak%20Banjir.jpeg" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Lokasi SPBU Terdampak Banjir</a>
-          <a href="https://arjunadigital.000webhostapp.com/pdf/2021.02.03%20FAQ%20PERTASHOP%20135%20V5.pdf" target="_blank" class="list-group-item list-group-item-action list-group-item-info">2021.02.03 FAQ PERTASHOP 135 V5</a>
           <a href="https://ops11.000webhostapp.com/img/Promo%20Sehat%20Hari%20Gizi%20Nasional.jpeg" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Promo Sehat Hari Gizi Nasional</a>
           <a href="https://arjunadigital.000webhostapp.com/pdf/Voucher%20Patraland%20Urbano_PDS%20135%20Bright%20Gas.pdf" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Bright Gas x Patra Jasa - Beli Apartemen Gratis Bright Gas Selama 1 Tahun</a>
-          <a href="https://arjunadigital.000webhostapp.com/pdf/Registrasi%20For%20Business.pdf" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Registrasi For Business</a>
+
           <a href="https://ops11.000webhostapp.com/img/Pertamina%20Delivery%20Service.jpeg" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Pertamina Delivery Service</a>
         </div>
+
+        <div class="list-group">
+          <h6 class="card-subtitle mb-2 text-muted mt-3">BBM</h6>
+          <a href="https://arjunadigital.000webhostapp.com/bbm/pdf/Pertamax%20dengan%20Formula%20Ecosave%20Technology.pdf" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Pertamax dengan Formula Ecosave Technology</a>
+        </div>
+
+        <div class="list-group">
+          <h6 class="card-subtitle mb-2 text-muted mt-3">DOMGAS</h6>
+          <a href="https://arjunadigital.000webhostapp.com/domgas/pdf/Product%20Knowledge%20Domestic%20Gas.pdf" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Product Knowledge Domestic Gas</a>
+        </div>
+        <div class="list-group">
+          <h6 class="card-subtitle mb-2 text-muted mt-3">MyPertamina</h6>
+          <a href="https://arjunadigital.000webhostapp.com/mypertamina/pdf/Registrasi%20For%20Business.pdf" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Registrasi For Business</a>
+        </div>
+
+        <div class="list-group">
+          <h6 class="card-subtitle mb-2 text-muted mt-3">SPBU</h6>
+          <a href="https://arjunadigital.000webhostapp.com/spbu/pdf/2021.02.03%20FAQ%20PERTASHOP%20135%20V5.pdf" target="_blank" class="list-group-item list-group-item-action list-group-item-info">2021.02.03 FAQ PERTASHOP 135 V5</a>
+        </div>
+        <div class="list-group">
+          <h6 class="card-subtitle mb-2 text-muted mt-3">Lubricant</h6>
+          <a href="https://arjunadigital.000webhostapp.com/lubricant/pdf/Harga%20Fastron%20di%20SPBU%20PDS.pdf" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Harga Fastron di SPBU PDS</a>
+        </div>
+
       </div>
     </div>
   </div>
@@ -21,16 +98,14 @@
         <h5 class="card-title text-center">Pertamina Call Center Application</h5>
         <h6 class="card-subtitle mb-2 text-muted mt-3">Ecommerce & Content</h6>
         <div class="list-group">
+          <a href="https://eppid.pertamina.com/" target="_blank" class="list-group-item list-group-item-action list-group-item-info">E-PPID Pertamina</a>
           <a href="https://kemitraan.pertamina.com/" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Kemitraan Pertamina</a>
           <a href="https://rensi135.000webhostapp.com/news" target="_blank" class="list-group-item list-group-item-action list-group-item-info">News archive</a>
           <a href="https://ops13.000webhostapp.com/index.php/promo" target="_blank" class="list-group-item list-group-item-action list-group-item-info">2021_01 Daftar Bright Store Promo Hari Gizi Nasional</a>
-          <a href="https://chanelsosmed.000webhostapp.com/xlsx/Arjuna%20-%20Updates%2018%20Februari%202021.xlsx" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Arjuna - Updates 18 Februari 2021</a>
           <a href="https://www.pertaminafuels.com/harga-dan-lokasi" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Harga BBM</a>
           <a href="https://recruitment.pertamina.com/" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Recruitment</a>
           <a href="https://drlube.pertaminalubricants.com/index.php/Home/" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Dr.LUBE</a>
-          <a href="https://spbumypertamina.000webhostapp.com/xlsx/list%20spbu%20produk.xlsx" target="_blank" class="list-group-item list-group-item-action list-group-item-info">List SPBU Produk</a>
           <a href="http://10.194.176.140/diana/login" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Diana</a>
-          <a href="https://spbumypertamina.000webhostapp.com/xlsx/Rekap%20SPBU%20MyPertamina%2003.02.21.xlsx" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Rekap SPBU MyPertamina 03.02.21</a>
           <a href="https://pds135.com/" target="_blank" class="list-group-item list-group-item-action list-group-item-info">PDS135</a>
           <a href="https://pds135.com/backoffice" target="_blank" class="list-group-item list-group-item-action list-group-item-info">PDS135 Backoffice</a>
           <a href="https://pertamina.com/" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Pertamina</a>
@@ -59,6 +134,7 @@
           <a href="https://brightgas.co.id/" target="_blank" class="list-group-item list-group-item-action list-group-item-info">Bright Gas</a>
           <a href="https://bbmsatuharga.com/" target="_blank" class="list-group-item list-group-item-action list-group-item-info">BBM Satu Harga</a>
         </div>
+
         <h6 class="card-subtitle mb-2 text-muted mt-3">Tools</h6>
         <div class="list-group">
           <a href="https://on4pertamina.infomedia.co.id/oct_pertamina" target="_blank" class="list-group-item list-group-item-action list-group-item-info">OMNI</a>
@@ -88,5 +164,6 @@
         </div>
       </div>
     </div>
+
   </div>
 </div>
